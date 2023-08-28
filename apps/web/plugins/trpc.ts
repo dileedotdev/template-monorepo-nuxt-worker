74463a08bd6d21794424a9cc0cc9f6756344ca75
dilee <me@dilee.dev>
2023-08-28 20:48:38 +0700

    fix type check

--- apps/web/plugins/trpc.ts
+++ apps/web/plugins/trpc.ts
@@ -1,18 +1,21 @@
-import type { AppRouter } from 'api/src/trpc.router'
 import { createTRPCNuxtClient } from 'trpc-nuxt/client'
 import { httpBatchLink } from '@trpc/client'
+import type { AppRouter } from '@resolvex/api/src/trpc.router'
+import type { FetchEsque } from '@trpc/client/dist/internals/types'
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
+
+  const fetchFn: FetchEsque = (...args) => {
+    // @ts-expect-error API_SERVICE is injected by Cloudflare Workers
+    return process.env.API_SERVICE?.fetch(...args) ?? fetch(...args)
+  }
 
   const trpc = createTRPCNuxtClient<AppRouter>({
     links: [
       httpBatchLink({
         url: config.public.trpcUrl,
-        fetch(...args: Parameters<typeof fetch>) {
-          // @ts-expect-error API_SERVICE is injected by Cloudflare Workers
-          return process.env.API_SERVICE?.fetch(...args) ?? fetch(...args)
-        },
+        fetch: fetchFn,
       }),
     ],
   })
